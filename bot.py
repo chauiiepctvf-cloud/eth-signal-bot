b765b1d0f0e4965ed40bb9d9a7d97cbacecd6bb9
chauiiepctvf-cloud <chauiiepctvf@gmail.com>
2026-04-19 16:38:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-```python
+
 import os
 import time
 import logging
@@ -463,4 +463,4 @@ if __name__ == "__main__":
     t.start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-```
+
